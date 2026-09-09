cb6124c3c873b71f0cb3f9fb45fb0e382c6edaa9
SchIza12 <schaeffer.izabella22k@egressy.info>
2026-09-09 09:41:54 +0200

    1. rész kész

---
+++ MethodsAndClasses/Book.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndClasses
+{
+    public class Book
+    {
+        public static int Count = 0;
+        public string Title { get; set; }
+        public string Author { get; set; }
+        private int pageCount { get; set; }
+        public int PageCount { get { return pageCount; } set { if (value < 0) { pageCount = 0; } else { pageCount = value; } } }
+        public bool isAvailabel { get; set; }
+
+        public Book(string Title, string Author, int pageCount) : this(Title,Author)
+        {
+            this.Title = Title;
+            this.Author = Author;
+            this.pageCount = pageCount;
+            Count++;
+            this.isAvailabel = true;
+        }
+        public Book(string Title,string Author) 
+        {
+            this.pageCount=0;
+        }
+        public void Describe()
+        {
+            Console.WriteLine($"{Title},{Author},{pageCount}");
+        }
+        public bool IsLong()
+        {
+            if (pageCount > 300)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool Borrow()
+        {
+            if (isAvailabel == true)
+            {
+                isAvailabel = false;
+                Console.WriteLine("kikölcsönözve");
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool Return()
+        {
+            if (isAvailabel == false)
+            {
+                isAvailabel = true;
+                Console.WriteLine("kikölcsönözve");
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ MethodsAndClasses/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace MethodsAndClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("Micimackó", "egy angol író",300);
+            Book book2 = new Book("Kockás fülű nyúl", "egy magyar író", -100);
+            Console.WriteLine(book1.Title,book2.Title);
+
+            book1.Describe();
+            book2.Describe();
+            book1.IsLong();
+            book2.IsLong();
+
+            Book book3 = new Book("kis vakond", "valami európai");
+            book2.Borrow();
+
+
+        }
+    }
+}
